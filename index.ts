4345393fc83f1b76a4afe6e573cd5412c504d71c
ruthrootz <ruthrootz@users.noreply.github.com>
2021-12-03 19:54:53 -0500

    u

--- index.ts
+++ index.ts
@@ -2,6 +2,16 @@ import * as crypto from 'crypto';
 
 class Transaction {
 
+  constructor(
+    public amount: number,
+    public payer: string, // public key
+    public payee: string // public key
+  ) {}
+
+  toString() {
+    return JSON.stringify(this);
+  }
+
 }
 
 class Block {
