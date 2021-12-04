d53aa098a849c7f35df935c2e10026e5d029652b
ruthrootz <ruthrootz@users.noreply.github.com>
2021-12-03 20:20:49 -0500

    u

--- index.ts
+++ index.ts
@@ -67,5 +67,13 @@ class Wallet {
   this.publicKey = keypair.publicKey;
   this.privateKey = keypair.privateKey;
 
+  sendMoney(amount: number, payeePublicKey: string) {
+    const transaction = new Transaction(amount, this.publicKey, payeePublicKey);
+    const sign = crypto.createSign('SHA256');
+    sign.update(transaction.toString()).end();
+    const signature = sign.sign(this.privateKey);
+    Blockchain.instance.addBlock(transaction, this.publicKey, signature);
+  }
+
 }
 
