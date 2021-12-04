898c62ed7f3c1619be8d9aca8490ea54590140a6
ruthrootz <ruthrootz@users.noreply.github.com>
2021-12-03 20:29:14 -0500

    u

--- index.ts
+++ index.ts
@@ -16,6 +16,8 @@ class Transaction {
 
 class Block {
 
+  public nonce = Math.round(Math.random() * 999999999);
+
   constructor(
     public previousHash: string,
     public transaction: Transaction,
@@ -45,8 +47,19 @@ class Chain {
     return this.chain[this.chain.length - 1];
   }
 
-  addBlock(transaction: Transaction, senderPublicKey: string, signature: string) {
+  mine(nonce: number) {
     
+  }
+
+  addBlock(transaction: Transaction, senderPublicKey: string, signature: string) {
+    const verifier = crypto.createVerify('SHA256');
+    verifier.update(transaction.toString());
+    const isValid = verifier.verify(senderPublicKey, signature);
+    if (isValid) {
+      const newBlock = new Block(this.lastBlock.hash, transaction);
+      this.mine(newBlock.nonce);
+      this.chain.push(newBlock);
+    }
   }
 
 }
